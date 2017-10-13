15418fd818076ad71741528a77639bd903b785e6
pizza <pizzahaut@gmail.com'>
2017-10-13 10:50:56 -0400

    made a branch for the non-recursive version

--- 15PuzzleSolverProto/src/Solver.java
+++ 15PuzzleSolverProto/src/Solver.java
@@ -2,13 +2,15 @@
  * @author Nathan Haut
  * project: 15 puzzle solver
  * date: 10/10/17
+ * 
+ * Non recursive, uses deque
 */
 public class Solver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Board a = new Board("123456789abcdef0",0);
-		a.scramble(10);
+		a.scramble(15);
 		//a = new Board("1234578c960bdaef",0);
 		//System.out.println(a.toString());
 		System.out.println("puzzle" + "\n" + "______" );
@@ -18,7 +20,7 @@ public class Solver {
 		System.out.println(puzz.substring(8, 12));
 		System.out.println(puzz.substring(12, 16));
 		long time = System.currentTimeMillis();
-		boolean solved = a.solveable(a.toString(),10);
+		boolean solved = a.solveable(a.toString(),15);
 		//System.out.println(solved);
 		//System.out.println(a.getSolvePath());
 		System.out.println("_______");
